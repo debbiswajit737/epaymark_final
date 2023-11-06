076b1817c38c8b374f6e2532385b2e4575f5d1d5
debbiswajit737 <debbiswajit737@gmail.com>
2023-11-06 19:11:19 +0530

    added new fragment for electric

---
+++ app/src/main/java/com/epaymark/epay/ui/fragment/ElectricRechargeFragment.kt
@@ -0,0 +1,85 @@
+package com.epaymark.epay.ui.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.epaymark.epay.R
+import com.epaymark.epay.data.model.UserInfoModel
+import com.epaymark.epay.data.viewMovel.MyViewModel
+import com.epaymark.epay.databinding.FragmentDthRechargeBinding
+import com.epaymark.epay.databinding.FragmentElectricRechargeBinding
+import com.epaymark.epay.ui.base.BaseFragment
+import com.epaymark.epay.ui.popup.CustomPopup.showBindingPopup
+
+class ElectricRechargeFragment : BaseFragment() {
+    lateinit var binding: FragmentElectricRechargeBinding
+    private val viewModel: MyViewModel by activityViewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_electric_recharge, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        setObserver()
+        onViewClick()
+    }
+
+    private fun onViewClick() {
+        binding.apply {
+
+            imgBack.back()
+
+            btnCustomerInfo.setOnClickListener{
+                context?.let {ctx->
+                    val userInfoModel=UserInfoModel("Sample Test User","178.00","23-04-2023","752")
+
+                    showBindingPopup(ctx,userInfoModel)
+                }
+            }
+
+            btnSubmit.setOnClickListener{
+                if (viewModel?.dthValidation() == true){
+                    Toast.makeText(btnSubmit.context, "ok", Toast.LENGTH_SHORT).show()
+                }
+            }
+            etOperator.setOnClickListener {
+                rlOperator.performClick()
+            }
+            rlOperator.setOnClickListener{
+                activity?.let {act->
+                    findNavController().navigate(R.id.action_DTHRechargeFragment_to_operatorFragment)
+                }
+
+            }
+        }
+
+
+
+    }
+
+    fun initView() {
+        binding.apply {
+            etAmt.setupAmount()
+        }
+
+    }
+
+    fun setObserver() {
+
+    }
+
+
+}
